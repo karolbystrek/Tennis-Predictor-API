ea2334f26a3954f1d4920f802c3fbf5c2d02a342
Karol Bystrek <karbystrek@gmail.com>
2025-04-26 21:16:33 +0200

    Update app config

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    print(f" * Loading environment variables from: {dotenv_path}")
+    load_dotenv(dotenv_path)
+else:
+    print(" * .env file not found, relying on system environment variables.")
+
 from app import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
-    debug_mode = app.config.get('DEBUG', False)
-    port = int(os.getenv('FLASK_RUN_PORT', 5000))
-    print(f" * Starting Flask server on http://{host}:{port}/ (Debug: {debug_mode})")
-    app.run(host=host, port=port, debug=debug_mode, use_reloader=debug_mode, threaded=True)
